8d03dfeee2e32002eb0fcf6e1c4ab6f8006216c3
luqiao <qiaolu@physik.hu-berlin.de>
2022-10-06 09:12:16 +0200

    second commit

--- git_test.java
+++ git_test.java
@@ -1,5 +1,6 @@
 public class git_test {
     public static void main(String[] args) {
         System.out.println("I miss you very much in the 1st day!");
+        System.out.println("I miss you very much in the 2st day!");
     }
 }
